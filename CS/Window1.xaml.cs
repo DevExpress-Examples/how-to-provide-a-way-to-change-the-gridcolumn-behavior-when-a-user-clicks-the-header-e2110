9bf342bf79878b93bafdbfcfa5be1a5db904ea43
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Window1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Documents;
+using DevExpress.Data;
+using DevExpress.Xpf.Grid;
+
+namespace DXGridTest {
+
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+
+            List<TestData> list = new List<TestData>();
+            for (int i = 0; i < 10; i++) {
+                list.Add(new TestData() 
+                    { Text = "Row" + i, Number = i });
+            }
+
+            grid.ItemsSource = list;
+        }
+    }
+
+    public class TestData {
+        public string Text { get; set; }
+        public int Number { get; set; }
+    }
+
+    public class MyTableView : TableView {
+        protected override void OnColumnHeaderClick(ColumnBase column, bool isShift, bool isCtrl) {
+            base.OnColumnHeaderClick(column, isShift, 
+                column.SortOrder == ColumnSortOrder.Descending);
+        }
+    }
+}
